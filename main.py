16ec4d10f795ea46640a2c9971807921e3a0c4aa
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2026-06-01 23:45:35 +0700

    Add middleware

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import engine, get_db
 import models
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Creates tables if they don't exist
 # Tells the session which database to talk to.
@@ -17,12 +25,18 @@ def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/horses")
-def read_horses(db: Session = Depends(get_db)):  
-    return db.query(models.Horse).all()
+def read_horses(db: Session = Depends(get_db)):
+    horses = db.query(models.Horse).all()
+    return horses
 
 @app.get("/horses/{horse_id}")
 def read_horse(horse_id: int, db: Session = Depends(get_db)):  # Depends calls get_db(), grabs the session, and injects it as db
-    return db.query(models.Horse).filter(models.Horse.id == horse_id).first()
+    horse = db.query(models.Horse).filter(models.Horse.id == horse_id).first()
+    
+    if horse is None:
+        raise HTTPException(status_code=404, detail="Horse not found")
+    
+    return horse
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
